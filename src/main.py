1100332692ffac192de1f298438ad59ebeb8390d
Andre Wiggins <drewigg@gmail.com>
2011-02-14 20:50:58 -0600

    added LICENSE and main.py

---
+++ src/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+# Name:        interpreter.py
+# Purpose:     Runs a WSGI server that allows the user to run Python code in
+#              in their browser.
+#
+# Author:      Andre Wiggins
+#
+# Created:     02/02/2011
+# Copyright:   (c) Andre Wiggins 2011
+# License:
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
