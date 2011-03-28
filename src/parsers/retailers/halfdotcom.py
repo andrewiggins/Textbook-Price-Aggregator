12a1f7a4724d4d4fffc38564f7dce16c75f217c9
Andre Wiggins <drewigg@gmail.com>
2011-03-28 00:38:12 -0500

    filled in halfdotcom.py header

--- src/parsers/retailers/halfdotcom.py
+++ src/parsers/retailers/halfdotcom.py
@@ -1,14 +1,26 @@
-#-------------------------------------------------------------------------------
-# Name:        HalfDotCom
-# Purpose:
-#
-# Author:      Andrew
-#
-# Created:     19/03/2011
-# Copyright:   (c) Andrew 2011
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
 #!/usr/bin/env python
+#-------------------------------------------------------------------------------
+# Name:        halfdotcom.py
+# Purpose:     Contains all parsing functions for half.com website
+#
+# Author:      Andrew Stewart
+#
+# Created:     03/19/2011
+# Copyright:   (c) Andre Wiggins, Jacob Marsh, Andrew Stewart 2011
+# License:
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#-------------------------------------------------------------------------------
 
 import BeautifulSoup,urllib2,parsers,traceback,data,time
 
