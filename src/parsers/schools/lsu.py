1601a34af7e94238da4ed429de8d9b9e2133b370
Andre Wiggins <drewigg@gmail.com>
2011-04-03 00:56:44 -0500

    quoted url parameters

--- src/parsers/schools/lsu.py
+++ src/parsers/schools/lsu.py
@@ -52,7 +52,7 @@ def get_terms():
 
 def get_options(term, dept='', course=''):
     url = 'http://lsu.bncollege.com/webapp/wcs/stores/servlet/TextBookProcessDropdownsCmd?campusId=17548053&termId=%s&deptId=%s&courseId=%s&sectionId=&storeId=19057&catalogId=10001&langId=-1&dojo.transport=xmlhttp&dojo.preventCache=1301120964177'
-    url = url % (term, dept, course)
+    url = url % tuple(map(urllib.quote, [term, dept, course]))
     try:
         html = urllib2.urlopen(url).read()
     except urllib2.HTTPError:
