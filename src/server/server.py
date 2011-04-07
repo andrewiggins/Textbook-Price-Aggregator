f0e66c5ee34ded9b91545558237fa5c7b66533e4
Andrew Stewart <andrewstewart25@gmail.com>
2011-04-06 22:02:34 -0500

    removed unneeded imports from server.py

--- src/server/server.py
+++ src/server/server.py
@@ -1,13 +1,5 @@
-import cgi
-import os
-import urllib
-from google.appengine.ext.webapp import template
-from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext import db
-from google.appengine.api import urlfetch
-import datetime,traceback
 
 class MainPage(webapp.RequestHandler):
 	def get(self):
