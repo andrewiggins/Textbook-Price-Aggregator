e48b9ca73780fd98b6d9ed984c59709c4cda669b
Andrew Stewart <andrewstewart25@gmail.com>
2011-04-06 20:19:30 -0500

    Added server.py

---
+++ src/server/server.py
@@ -0,0 +1,25 @@
+import cgi
+import os
+import urllib
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+from google.appengine.api import urlfetch
+import datetime,traceback
+
+class MainPage(webapp.RequestHandler):
+	def get(self):
+		self.response.out.write("HI THERE")
+		
+		
+application = webapp.WSGIApplication(
+                                     [('/',MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
