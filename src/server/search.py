240893ca11427a644307ec2f737e8b52e03f4547
Andrew Stewart <andrewstewart25@gmail.com>
2011-04-28 18:55:36 -0500

    blah

--- src/server/search.py
+++ src/server/search.py
@@ -44,7 +44,7 @@ class SearchResultsPage(webapp.RequestHandler):
             else:
                 path = '../static/templates/search.html'                
                 retailer = "halfdotcom"
-                newurl = "search/%s?q=%s&type=%s"%(retailer,query,type)
+                newurl = "/search/%s?q=%s&type=%s"%(retailer,query,type)
                 url2 = "/book"
                 template_values={"url":newurl,"url2":url2,"retailer":retailer}
                 self.response.out.write(template.render(path, template_values, True))             
