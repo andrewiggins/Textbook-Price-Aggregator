771a2ffffe9df7c972beac27250f53354eac61fc
Andre Wiggins <drewigg@gmail.com>
2011-04-29 17:29:03 -0500

    added headers to html and js files, changed base.css to styles.css

--- src/static/js/datafuncs.js
+++ src/static/js/datafuncs.js
@@ -1,8 +1,27 @@
 /**
- * This javascript file contains javascript methods pertaining to the getting,
- * processing, and outputing of SWAT Textbooks data.
+ *------------------------------------------------------------------------------
+ * Name:        datafuncs.js
+ * Purpose:     This javascript file contains javascript methods pertaining to 
+ *              the getting, processing, and outputing of SWAT Textbooks data.
+ *
+ * Author(s):   Andre Wiggins, Andrew Stewart
+ *
+ * Created:     04/22/2011
+ * Copyright:   (c) Jacob Marsh, Andrew Stewart, Andre Wiggins 2011
+ * License:
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  * 
- * Requires the util file: util.js
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * -----------------------------------------------------------------------------
  */
 
 
